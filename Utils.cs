79fd6a3a2f4e62f8d0da40c5a0561860bfb36043
Mil0w0 <lhilderal@myges.fr>
2025-05-04 19:30:20 +0200

    formatting command

--- Utils.cs
+++ Utils.cs
@@ -18,4 +18,28 @@ public class Utils
         Console.ResetColor();
         return input;
     }
+
+    // The factory wants to group all same robot types together when an instruction is run.
+    public static Dictionary<string, int> FilterCommand(string input)
+    {
+        Dictionary<string, int> robotQuantities = new Dictionary<string, int>();
+        string[] robots = input.Split(",");
+        foreach (var robot in robots)
+        {
+            string[] robotCommand = robot.Split(" ");
+            string quantity = robotCommand[0];
+            string robotName = robotCommand[1];
+            
+            if (robotQuantities.ContainsKey(robotName))
+            {
+                robotQuantities[robotName] += int.Parse(quantity);
+            }
+            else
+            {
+                robotQuantities.Add(robotName, int.Parse(quantity));
+            }
+        }
+
+        return robotQuantities;
+    }
 }
